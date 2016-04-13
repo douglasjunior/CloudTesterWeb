554e37ccfacc05062dea799b490abfb5030c6c40
Douglas Nassif Roma Junior <nassifrroma@gmail.com>
2016-04-13 11:54:36 -0300

    - Implementado upload de arquivos e salvamento em disco. - Adicionado controle de transação.

--- src/main/java/br/edu/utfpr/cp/cloudtesterweb/controller/IndexController.java
+++ src/main/java/br/edu/utfpr/cp/cloudtesterweb/controller/IndexController.java
@@ -2,13 +2,19 @@ package br.edu.utfpr.cp.cloudtesterweb.controller;
 
 import br.edu.utfpr.cp.cloudtesterweb.model.FileEntity;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.io.Serializable;
+import javax.annotation.Resource;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
+import javax.transaction.UserTransaction;
+import org.eclipse.persistence.tools.file.FileUtil;
 import org.primefaces.model.UploadedFile;
+import static br.edu.utfpr.cp.cloudtesterweb.util.Constants.*;
 
 /**
  *
@@ -20,6 +26,9 @@ public class IndexController implements Serializable {
 
     @Inject
     private Dao dao;
+
+    @Resource
+    private UserTransaction tx;
 
     public IndexController() {
     }
@@ -35,15 +44,32 @@ public class IndexController implements Serializable {
     }
 
     public void upload() {
-        if (file != null) {
+        try {
+            tx.begin();
+            File directory = new File(UPLOADED_FOLDER);
+            directory.mkdirs();
+            File fileOut = new File(directory, file.getFileName());
+            try (FileOutputStream fos = new FileOutputStream(fileOut);
+                    InputStream is = file.getInputstream();) {
+                FileUtil.copy(is, fos);
+            }
             FileEntity entity = new FileEntity();
             entity.setName(file.getFileName());
             entity.setContentLength(file.getSize());
             entity.setContentType(file.getContentType());
-            entity.setContentPath("/caminho/arquivo.txt");
+            entity.setContentPath(fileOut.getAbsolutePath());
             dao.insert(entity);
-            System.out.println(new File("teste.txt").getAbsolutePath());
-            FacesMessage message = new FacesMessage("Succesful", file.getFileName() + " is uploaded.");
+            tx.commit();
+            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Succesful", file.getFileName() + " is uploaded.");
+            FacesContext.getCurrentInstance().addMessage(null, message);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            try {
+                tx.rollback();
+            } catch (Exception ex1) {
+                ex1.printStackTrace();
+            }
+            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", file.getFileName() + " is not uploaded.");
             FacesContext.getCurrentInstance().addMessage(null, message);
         }
     }
---
+++ src/main/java/br/edu/utfpr/cp/cloudtesterweb/util/Constants.java
@@ -0,0 +1,14 @@
+package br.edu.utfpr.cp.cloudtesterweb.util;
+
+/**
+ *
+ * @author douglas
+ */
+public final class Constants {
+
+    public static final String APP_FOLDER = "CloudTesterWeb";
+    public static final String UPLOADED_FOLDER = APP_FOLDER + "/uploaded";
+
+    private Constants() {
+    }
+}
